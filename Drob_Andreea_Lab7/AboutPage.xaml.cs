6a3d8ff3f5cef4205aa3183b65fe95bbfab0eeea
drobandreea <105298940+drobandreea@users.noreply.github.com>
2022-11-28 22:09:30 -1200

    Add project files.

---
+++ Drob_Andreea_Lab7/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Drob_Andreea_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Drob_Andreea_Lab7/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Drob_Andreea_Lab7;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
