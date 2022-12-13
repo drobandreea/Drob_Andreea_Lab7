eb4592730e3254870dce9f0df092757017c288dd
drobandreea <105298940+drobandreea@users.noreply.github.com>
2022-12-12 15:26:50 -1200

    change

--- Drob_Andreea_Lab7/Data/ShoppingListDatabase.cs
+++ Drob_Andreea_Lab7/Data/ShoppingListDatabase.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using SQLite;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,7 +7,7 @@ using System.Text;
 using Drob_Andreea_Lab7.Models;
 
 namespace Drob_Andreea_Lab7.Data
-{
+   {
     public class ShoppingListDatabase
     {
         readonly SQLiteAsyncConnection _database;
@@ -16,6 +15,8 @@ namespace Drob_Andreea_Lab7.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
         public Task<List<ShopList>> GetShopListsAsync()
         {
@@ -42,5 +43,46 @@ namespace Drob_Andreea_Lab7.Data
         {
             return _database.DeleteAsync(slist);
         }
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
+
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+            "select P.ID, P.Description from Product P"
+            + " inner join ListProduct LP"
+            + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+            shoplistid);
+        }
+
     }
 }
